adec2d0e46bf89544e94146bd2c9f6bdc65a2800
jexxxxxx <jexx.crypto@gmail.com>
2025-10-22 14:10:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration file for Sad Cat
 const CONFIG = {
     // Contract Address - update this when the contract is deployed
-    contractAddress: "keynd is gay",
+    contractAddress: "keynd is super gay",
     
 };
 
