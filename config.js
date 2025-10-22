984f9a8f15a004c484ee1387578481f7015cea8d
keynd77 <keynd77@gmail.com>
2025-10-22 22:57:41 +0200

    Add config system for contract address management
    - Created config.js for centralized configuration
    - Updated index.html to load contract address dynamically
    - Contract address can now be easily changed in config.js
    - Added JavaScript to display contract address from config

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuration file for Sad Cat
+const CONFIG = {
+    // Contract Address - update this when the contract is deployed
+    contractAddress: "coming soon",
+    
+};
+
+window.CONFIG = CONFIG;
