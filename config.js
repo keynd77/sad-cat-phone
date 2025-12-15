9ff0af471a3c1fe5e501be5c91f1a2368d1ea294
keynd77 <keynd77@gmail.com>
2025-12-15 23:25:12 +0100

    Update background to dark gray and adjust meme canvas border

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration file for Sad Cat
 const CONFIG = {
     // Contract Address - update this when the contract is deployed
-    contractAddress: "keynd is super g4y",
+    contractAddress: "coming soon",
     
 };
 
